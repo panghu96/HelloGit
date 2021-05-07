2512379cb3a6f8112df102218ce2ce13cefed389
panghu <1062571616@qq.com>
2021-05-07 10:33:30 +0800

    创建本地库

---
+++ test01/src/main/java/com/aura/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.aura.git;
+
+/**
+ * Author:panghu
+ * Date:2021-05-07
+ * Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
